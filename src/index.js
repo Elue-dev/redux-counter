f032e08a814d54e339f6e2e1f14767c1fb1a4d8d
Elue-dev <eluewisdom@gmail.com>
2021-10-05 17:22:10 +0100

    first-commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import allReducers from './reducers'
+import { createStore }  from 'redux'
+import { Provider } from 'react-redux'
+
+const myStore = createStore(allReducers)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {myStore} >
+     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
